10274f0b9fb5cb769be7c58b39e067de056359ac
Khai Truong <kd.kidr@gmail.com>
2021-05-04 12:07:05 +0700

    First commit

---
+++ src/main/java/s3818074/cosc2440a2/Cosc2440A2Application.java
@@ -0,0 +1,13 @@
+package s3818074.cosc2440a2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cosc2440A2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cosc2440A2Application.class, args);
+	}
+
+}
---
+++ src/test/java/s3818074/cosc2440a2/Cosc2440A2ApplicationTests.java
@@ -0,0 +1,13 @@
+package s3818074.cosc2440a2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cosc2440A2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
