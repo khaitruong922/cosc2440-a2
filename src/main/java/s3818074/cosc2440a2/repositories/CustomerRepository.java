9f7cc53a56422485b99810d386dbadf4b28baf8f
Khai Truong <kd.kidr@gmail.com>
2021-05-07 15:05:27 +0700

    Adding customer search functions

--- src/main/java/s3818074/cosc2440a2/repositories/CustomerRepository.java
+++ src/main/java/s3818074/cosc2440a2/repositories/CustomerRepository.java
@@ -4,8 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import s3818074.cosc2440a2.models.Customer;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    List<Customer> findAllByName(String name);
+
+    List<Customer> findAllByAddress(String address);
+
+    List<Customer> findAllByPhone(String phone);
+
 }
