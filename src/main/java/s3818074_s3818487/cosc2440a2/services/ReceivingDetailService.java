db91ebc35b57ccdec00f1f41c5257cafc594bb81
Khai Truong <kd.kidr@gmail.com>
2021-05-18 11:06:13 +0700

    doing receiving note and receiving details

--- src/main/java/s3818074_s3818487/cosc2440a2/services/ReceivingDetailService.java
+++ src/main/java/s3818074_s3818487/cosc2440a2/services/ReceivingDetailService.java
@@ -2,17 +2,31 @@ package s3818074_s3818487.cosc2440a2.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import s3818074_s3818487.cosc2440a2.models.Product;
 import s3818074_s3818487.cosc2440a2.models.ReceivingDetail;
+import s3818074_s3818487.cosc2440a2.repositories.ProductRepository;
 import s3818074_s3818487.cosc2440a2.repositories.ReceivingDetailRepository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @Transactional
 public class ReceivingDetailService extends AbstractService<ReceivingDetail, UUID> {
+    private final ProductRepository productRepository;
+
     @Autowired
-    public ReceivingDetailService(ReceivingDetailRepository repo) {
+    public ReceivingDetailService(ReceivingDetailRepository repo, ProductRepository productRepository) {
         super(repo);
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ReceivingDetail add(ReceivingDetail receivingDetail) {
+        Optional<Product> productOptional = productRepository.findById(receivingDetail.getProduct().getId());
+        if (productOptional.isEmpty()) return null;
+        receivingDetail.setProduct(productOptional.get());
+        return super.add(receivingDetail);
     }
 }
--- src/main/java/s3818074_s3818487/cosc2440a2/services/ReceivingNoteService.java
+++ src/main/java/s3818074_s3818487/cosc2440a2/services/ReceivingNoteService.java
@@ -2,24 +2,31 @@ package s3818074_s3818487.cosc2440a2.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import s3818074_s3818487.cosc2440a2.models.ReceivingDetail;
 import s3818074_s3818487.cosc2440a2.models.ReceivingNote;
 import s3818074_s3818487.cosc2440a2.models.Staff;
+import s3818074_s3818487.cosc2440a2.repositories.ProductRepository;
+import s3818074_s3818487.cosc2440a2.repositories.ReceivingDetailRepository;
 import s3818074_s3818487.cosc2440a2.repositories.ReceivingNoteRepository;
 import s3818074_s3818487.cosc2440a2.repositories.StaffRepository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class ReceivingNoteService extends AbstractService<ReceivingNote, UUID> {
     private final StaffRepository staffRepository;
+    private final ReceivingDetailRepository receivingDetailRepository;
 
     @Autowired
-    public ReceivingNoteService(ReceivingNoteRepository repo, StaffRepository staffRepository) {
+    public ReceivingNoteService(ReceivingNoteRepository repo, StaffRepository staffRepository, ProductRepository productRepository, ReceivingDetailRepository receivingDetailRepository) {
         super(repo);
         this.staffRepository = staffRepository;
+        this.receivingDetailRepository = receivingDetailRepository;
     }
 
     @Override
@@ -27,6 +34,12 @@ public class ReceivingNoteService extends AbstractService<ReceivingNote, UUID> {
         Optional<Staff> staffOptional = staffRepository.findById(receivingNote.getStaff().getId());
         if (staffOptional.isEmpty()) return null;
         receivingNote.setStaff(staffOptional.get());
+        List<ReceivingDetail> receivingDetails = receivingNote.getReceivingDetails();
+        receivingDetails = receivingDetails.stream().filter(detail -> {
+            Optional<ReceivingDetail> detailOptional = receivingDetailRepository.findById(detail.getId());
+            return detailOptional.isPresent();
+        }).collect(Collectors.toList());
+        receivingNote.setReceivingDetails(receivingDetails);
         return super.add(receivingNote);
     }
 }
